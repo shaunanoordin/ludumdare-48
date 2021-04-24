3939f0ac687afd83a74c67ceabe27b67d96a3bf3
Shaun A. Noordin <shaun.a.noordin@gmail.com>
2021-04-24 18:07:13 +0100

    Remove bouncing

--- src/ld48/entity.js
+++ src/ld48/entity.js
@@ -118,60 +118,8 @@ class Entity {
   }
   
   onCollision (target, collisionCorrection) {
-    // when two solid shapes collide, bounce!
-    if (
-      this.movable && this.solid
-      && !target.movable && target.solid
-    ) {
-      if (
-        this.shape === SHAPES.CIRCLE && target.shape === SHAPES.CIRCLE
-      ) {
-        
-        // For circle + circle collisions, the collision correction already
-        // tells us the bounce direction.
-        const angle = Math.atan2(collisionCorrection.y - this.y, collisionCorrection.x - this.x)
-        const speed = Math.sqrt(this.speedX * this.speedX + this.speedY * this.speedY)
-
-        this.speedX = Math.cos(angle) * speed
-        this.speedY = Math.sin(angle) * speed
-
-      } else if (
-        this.shape === SHAPES.CIRCLE
-        && (target.shape === SHAPES.SQUARE || target.shape === SHAPES.POLYGON)
-      ) {
-        
-        // For circle + polygon collisions, we need to know...
-        // - the original angle this circle was moving towards (or rather, its
-        //   reverse, because we want a bounce)
-        // - the normal vector (of the edge) of the polygon this circle collided
-        //   into (which we can get from the collision correction)
-        // - the angle between them
-        const reverseOriginalAngle = Math.atan2(-this.speedY, -this.speedX)
-        const normalAngle = Math.atan2(collisionCorrection.y - this.y, collisionCorrection.x - this.x)
-        const angleBetween = normalAngle - reverseOriginalAngle
-        const angle = reverseOriginalAngle + 2 * angleBetween
-
-        const speed = Math.sqrt(this.speedX * this.speedX + this.speedY * this.speedY)
-
-        this.speedX = Math.cos(angle) * speed
-        this.speedY = Math.sin(angle) * speed
-        
-      } else {
-        // For the moment, we're not too concerned about polygons bumping into each other
-      }
-    } else if (
-      this.movable && this.solid
-      && target.movable && target.solid
-      && collisionCorrection.speedX !== undefined
-      && collisionCorrection.speedY !== undefined
-    ) {
-      this.speedX = collisionCorrection.speedX
-      this.speedY = collisionCorrection.speedY
-    }
-    
     this.x = collisionCorrection.x
     this.y = collisionCorrection.y
-    
   }
   
   get left () { return this.x - this.size / 2 }
